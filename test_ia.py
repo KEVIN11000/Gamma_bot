943acc50d342708ecba0a7af76d87e5ecae595f0
Kevin11000 <109119145+KEVIN11000@users.noreply.github.com>
2026-08-10 10:00:40 -0300

    Delete test_ia.py

--- test_ia.py
+++
@@ -1,6 +0,0 @@
-from logic.logic import AgenteAutonomoHoras
-
-agente = AgenteAutonomoHoras(spreadsheet_id="1hIDfS4Kfc-T9F-51LBBF5N0YqLaN2hibjxXyctiI-aA")
-resultado = agente.interpretar_frase_con_ia("tengo que entregar el laboratorio de mecatrónica mañana a las 4 y media de la tarde")
-
-print(resultado)
